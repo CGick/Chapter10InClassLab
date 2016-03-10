3b4c31f0028b5824c0c153ad6512f78d9c7c4369
SK207_29 <SK207_29@K207_W829.carrollcc.edu>
2016-03-09 20:10:53 -0500

    added decimal format

--- Chapter10InClassLab/src/chapter10/InClass/Lab/Shape.java
+++ Chapter10InClassLab/src/chapter10/InClass/Lab/Shape.java
@@ -1,8 +1,10 @@
 package chapter10.InClass.Lab;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
 
 public interface Shape extends Serializable{
 
+	DecimalFormat format = new DecimalFormat("%,.2f");
 	double getArea();
 }
