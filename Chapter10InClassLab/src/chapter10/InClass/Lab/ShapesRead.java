c0c63c5832129ab9fd9697a98b73d02eb64247ca
Paul Turnbaugh <pet30200@gmail.com>
2016-03-09 20:35:39 -0500

    Updated ShapesRead

--- Chapter10InClassLab/src/chapter10/InClass/Lab/ShapesRead.java
+++ Chapter10InClassLab/src/chapter10/InClass/Lab/ShapesRead.java
@@ -3,6 +3,7 @@ package chapter10.InClass.Lab;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 
 public class ShapesRead {
@@ -11,11 +12,11 @@ public class ShapesRead {
 		FileInputStream fis = new FileInputStream("shapes.dat");
 		ObjectInputStream input = new ObjectInputStream(fis);
 		
-		Shape[] shapesData = new Shape[5];
+		ArrayList<Shape> shapesData = new ArrayList<>();
 		
 		for(int i = 0; i < shapesData.length; i++){
 			try {
-				shapesData[i] = (Shape)input.readObject();
+				shapesData.add = (Shape)input.readObject();
 			} catch (ClassNotFoundException e) {
 				System.err.println("Tried to read a shape but that class does not exist!");
 				e.printStackTrace();
